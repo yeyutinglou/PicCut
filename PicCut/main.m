d0ace53726127c2813df4cfdb4a5a90d9d8b3189
duyuwei <yeyutinglou@gmail.com>
2019-03-24 09:17:25 +0800

    Initial Commit

---
+++ PicCut/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PicCut
+//
+//  Created by 杜玉伟 on 2019/3/24.
+//  Copyright © 2019年 杜玉伟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
